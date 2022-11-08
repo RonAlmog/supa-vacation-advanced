685c9e0f5a329661f8a12264dfa4e3271c063cea
Ron Almog <ron8132@gmail.com>
2022-11-08 12:43:37 -0500

    add edit page, save function

--- pages/api/homes/[id].js
+++ pages/api/homes/[id].js
@@ -1,5 +1,11 @@
 import { getSession } from "next-auth/react";
 import { prisma } from "../../../src/prisma";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
@@ -31,9 +37,25 @@ export default async function handler(req, res) {
       res.status(500).json({ message: "Something went wrong" });
     }
   }
+
+  if (req.method === "DELETE") {
+    try {
+      const home = await prisma.home.delete({
+        where: { id },
+      });
+      // remove image from supabase storage
+      if (home.image) {
+        const path = home.image.split(`${process.env.SUPABASE_BUCKET}/`)?.[1];
+        await supabase.storage.from(process.env.SUPABASE_BUCKET).remove([path]);
+      }
+      res.status(200).json(home);
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
   // HTTP method not supported!
   else {
-    res.setHeader("Allow", ["PATCH"]);
+    res.setHeader("Allow", ["PATCH", "DELETE"]);
     res
       .status(405)
       .json({ message: `HTTP method ${req.method} is not supported.` });
--- pages/homes/[id].js
+++ pages/homes/[id].js
@@ -1,8 +1,53 @@
+import { useEffect } from "react";
 import Image from "next/image";
 import Layout from "@/components/Layout";
 import { prisma } from "../../src/prisma";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
 
 const ListedHome = (home = null) => {
+  const { data: session } = useSession();
+  const [isOwner, setIsOwner] = useState(false);
+  const router = useRouter();
+
+  const [deleting, setDeleting] = useState(false);
+
+  const deleteHome = async () => {
+    let toastId;
+    try {
+      toastId = toast.loading("Deleting...");
+      setDeleting(true);
+      // delete from db
+      await axios.delete(`/api/homes/${home.id}`);
+      toast.success("Successfully deleted", { id: toastId });
+      router.push("/homes");
+    } catch (error) {
+      console.log(error);
+      toast.error("Unable to delete home", { id: toastId });
+      setDeleting(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (session?.user) {
+        try {
+          const owner = await axios.get(`/api/homes/${home.id}/owner`);
+          console.log("owner", owner);
+          console.log("sesssion", session);
+          setIsOwner(owner?.id === session.user.id);
+          // setIsOwner(false);
+          console.log("isOwner:", isOwner);
+        } catch (error) {
+          setIsOwner(false);
+        }
+      }
+    })();
+  }, [session?.user]);
+
   return (
     <Layout>
       <div className="max-w-screen-lg mx-auto">
@@ -25,6 +70,29 @@ const ListedHome = (home = null) => {
               </li>
             </ol>
           </div>
+
+          {isOwner ? (
+            <div className="flex items-center space-x-2">
+              <button
+                type="button"
+                onClick={() => router.push(`/homes/${home.id}/edit`)}
+                className="px-4 py-1 border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition rounded-md"
+              >
+                Edit
+              </button>
+
+              <button
+                type="button"
+                onClick={deleteHome}
+                disabled={deleting}
+                className="rounded-md border border-rose-500 text-rose-500 hover:bg-rose-500 hover:text-white focus:outline-none transition disabled:bg-rose-500 disabled:text-white disabled:opacity-50 disabled:cursor-not-allowed px-4 py-1"
+              >
+                {deleting ? "Deleting..." : "Delete"}
+              </button>
+            </div>
+          ) : (
+            <p>nope</p>
+          )}
         </div>
         <div className="mt-6 relative aspect-w-16 aspect-h-9 bg-gray-200 rounded-lg shadow-md overflow-hidden">
           {home?.image ? (
